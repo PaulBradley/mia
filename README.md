7dcf9d28ae5dac7ab78f0f4de43d59f9446aa0c8
Paul Bradley <contact@paulbradley.org>
2022-05-01 07:40:29 +0100

    updated README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MIA - Medical Imaging Acquisition Application
 
-MIA is a serverless web application that allows NHS physicians to register patients and send them a secure URL via SMS to upload photos and videos for clinical review. The patient receives the secure URL and needs to authenticate themselves by confirming their date of birth. Once authenticated, the patient can select images and videos they wish the physician to review. The patient can also enter a description of the behaviour or condition they want to be reviewed. Then, the media is uploaded directly into a private S3 bucket using a short-lived signed URL, and the physician is notified to check the submitted media.
+MIA is a serverless web application that allows NHS physicians to register patients and send them a secure URL via SMS so that the patient can upload photos and videos securely clinical review. The patient receives the secure URL and needs to authenticate themselves by confirming their date of birth. Once authenticated, the patient can select which images and videos they wish the physician to review. The patient can also enter a description of the behaviour or condition they want to be reviewed. The media is uploaded directly into a private S3 bucket using a short-lived signed URL, and the physician is notified to check the submitted media.
 
 Once the physician has reviewed the uploaded media, they can publish it to a Health Information Exchange using an ITI-41  Provide and Register Set-b message.
 
@@ -8,5 +8,15 @@ Once the physician has reviewed the uploaded media, they can publish it to a Hea
 
 The Terraform scripts deploy and provision the following architecture:
 
-![Architecture](/img/architecture.png?raw=true)
+![Architecture](/img/mia-cloud-architecture-diagram.svg?raw=true)
 
+## Video Demonstration
+
+To understand this applications clinical workflow, please watch the five-minute video below.
+
+[![MIA - Medical Imaging Acquisition Application](https://img.youtube.com/vi/U5IAxRy8dSw/0.jpg)](https://www.youtube.com/watch?v=U5IAxRy8dSw)
+
+
+## Technical Write Up
+
+A comprehensive technical write up can [be found here](https://paulbradley.dev/mia/)
--- database/schema.sql
+++ database/schema.sql
@@ -4,57 +4,87 @@
 
 SET FOREIGN_KEY_CHECKS=0;
 
+-- ORGANISATIONS
+    DROP TABLE IF EXISTS mia_organisations;
+
+    CREATE TABLE mia_organisations (
+        ods_code            CHAR(6)   NOT NULL,
+        organisation_name   VARCHAR(255) NOT NULL,
+
+        PRIMARY KEY (ods_code)
+    );
+
+
 -- USERS
-    DROP INDEX IF EXISTS idx_mia_users_user_name;
+    DROP INDEX IF EXISTS idx_mia_users_email_session;
     DROP TABLE IF EXISTS mia_users;
 
     CREATE TABLE mia_users (
-        user_name               VARCHAR(15)  NOT NULL UNIQUE,
-        organisation_ods_code   CHAR(3)      NOT NULL,
-        title                   VARCHAR(15)  NOT NULL,
-        given_name              VARCHAR(50)  NOT NULL,
-        family_name             VARCHAR(50)  NOT NULL,
-        email                   VARCHAR(255) NOT NULL,
-        password_hash           VARCHAR(128) NOT NULL
+        email_account       CHAR(128)   NOT NULL,
+        email_address       BLOB        NOT NULL,
+        ods_code            CHAR(6)     NOT NULL,
+        speciality          VARCHAR(50) NOT NULL,
+        title               BLOB        NOT NULL,
+        given_name          BLOB        NOT NULL,
+        family_name         BLOB        NOT NULL,
+        job_title           BLOB        NOT NULL,
+        password_hash       CHAR(128)   NOT NULL,
+        totp_secret         BLOB,
+        totp_qr_code        BLOB,
+        onboardingEmailSent TIMESTAMP,
+        session_id          CHAR(128),
+
+        PRIMARY KEY (email_account),
+        FOREIGN KEY (ods_code) REFERENCES mia_organisations(ods_code)
     );
 
-    CREATE INDEX idx_mia_users_user_name ON mia_users(user_name);
+    CREATE INDEX idx_mia_users_email_session ON mia_users(email_account, session_id);
 
 
 -- ENCOUNTERS
     DROP INDEX IF EXISTS idx_mia_encounters_dt_created;
+    DROP INDEX IF EXISTS idx_mia_encounters_care_unit;
+    DROP INDEX IF EXISTS idx_mia_encounters_care_unit_dt_created;
     DROP TABLE IF EXISTS mia_encounters;
 
     CREATE TABLE mia_encounters (
-        encounter_id            CHAR(16)     NOT NULL UNIQUE,
-        patient_id              CHAR(10)     NOT NULL,
-        patient_birthDate       DATE         NOT NULL,
-        patient_familyName      VARCHAR(50)  NOT NULL,
-        patient_gender          CHAR(1)      NOT NULL,
-        patient_givenName       VARCHAR(50)  NOT NULL,
-        patient_mobile_number   CHAR(13),
-        created_by_user_name    VARCHAR(15),
-        behaviour_description   TEXT,
+        encounter_id            CHAR(16)    NOT NULL,
+        care_unit               CHAR(10)    NOT NULL,
+        patient_id              CHAR(10)    NOT NULL,
+        patient_birthDate       BLOB        NOT NULL,
+        patient_familyName      BLOB        NOT NULL,
+        patient_gender          CHAR(1)     NOT NULL,
+        patient_givenName       BLOB        NOT NULL,
+        patient_mobile_number   BLOB,
+        created_by              CHAR(128),
+        behaviour_description   BLOB,
+        consent_diagnosis       CHAR(1)     NOT NULL DEFAULT 'N',
+        consent_patient_record  CHAR(1)     NOT NULL DEFAULT 'N',
+        consent_research        CHAR(1)     NOT NULL DEFAULT 'N',
         dt_consented            DATETIME,
         dt_created              DATETIME,
         dt_opened               DATETIME,
         dt_sms_sent             DATETIME,
         dt_submitted            DATETIME,
+        dt_deleted              DATETIME,
         dt_sent_health_exchange DATETIME,
         dt_ack_health_exchange  DATETIME,
 
-        FOREIGN KEY (created_by_user_name) REFERENCES mia_users(user_name)
+        PRIMARY KEY (encounter_id),
+        FOREIGN KEY (created_by) REFERENCES mia_users(email_account)
     );
 
-    CREATE INDEX idx_mia_encounters_dt_created ON mia_encounters(dt_created);
+    CREATE INDEX idx_mia_encounters_care_unit ON mia_encounters(care_unit);
+    CREATE INDEX idx_mia_encounters_care_unit_dt_created ON mia_encounters(care_unit, dt_created);
 
 
 -- MEDIA
     DROP INDEX IF EXISTS idx_mia_media_dt_uploaded;
+    DROP INDEX IF EXISTS idx_mia_media_mimetype;
     DROP TABLE IF EXISTS mia_media;
 
     CREATE TABLE mia_media (
-        objectkey               CHAR(128)    NOT NULL UNIQUE,
+        objectkey               CHAR(128)    NOT NULL,
         mimetype                CHAR(10)     NOT NULL,
         original_filename       VARCHAR(255) NOT NULL,
         file_exentions          VARCHAR(10)  NOT NULL,
@@ -62,11 +92,17 @@ SET FOREIGN_KEY_CHECKS=0;
         encounter_id            CHAR(16)     NOT NULL,
         dt_uploaded             DATETIME,
         dt_deleted              DATETIME,
+        image_width             INT NOT NULL DEFAULT 0,
+        image_height            INT NOT NULL DEFAULT 0,
 
+        PRIMARY KEY (objectkey),
         FOREIGN KEY (encounter_id) REFERENCES mia_encounters(encounter_id)
     );
 
+    CREATE INDEX idx_mia_media_mimetype ON mia_media(encounter_id, mimetype);
     CREATE INDEX idx_mia_media_dt_uploaded ON mia_media(objectkey, dt_uploaded);
 
 
+
 SET FOREIGN_KEY_CHECKS=1;
+SHOW TABLES;
